2f45b4c6498b73fab10bb770deb06f885b04fcde
Sabri <akrir.sabri2006@gmail.com>
2025-01-07 15:51:48 +0100

    Lien-dev

---
+++ src/app/mes-liens/mes-liens.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-liens',
+  standalone: true,
+  imports: [],
+  templateUrl: './mes-liens.component.html',
+  styleUrl: './mes-liens.component.scss'
+})
+export class MesLiensComponent {
+
+}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../logo/logo.component";
+import { MesLiensComponent } from "../mes-liens/mes-liens.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, MesLiensComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
