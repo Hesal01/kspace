11ca3ea030a1aa2b463abd082ccad5c737e19874
Sabri <akrir.sabri2006@gmail.com>
2025-01-09 14:46:07 +0100

    add_hamburger_mobile

--- src/app/mes-liens/mes-liens.component.ts
+++ src/app/mes-liens/mes-liens.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mes-liens',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mes-liens.component.html',
   styleUrl: './mes-liens.component.scss'
 })
 export class MesLiensComponent {
-
+  isclicked = false
+  openMenu(){
+    this.isclicked = ! this.isclicked
+  }
 }
 
 
