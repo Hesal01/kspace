e890b2608f3d5f64ad7319c039db54472fecc4a5
Sabri <akrir.sabri2006@gmail.com>
2025-01-09 10:21:10 +0100

    hamburger_links

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { LogoComponent } from "../logo/logo.component";
-import { MesLiensComponent } from "../mes-liens/mes-liens.component";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { LogoComponent } from "../logo/logo.component";  
+import { MesLiensComponent } from "../mes-liens/mes-liens.component"; 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LogoComponent, MesLiensComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
+  imports: [LogoComponent, MesLiensComponent]
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
+  @ViewChild('hamburger') hamburger: ElementRef | undefined;
+  @ViewChild('navLinks') navLinks: ElementRef | undefined;
+  nativeElement: any;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    
+    if (this.hamburger && this.navLinks) {
+      this.hamburger.nativeElement.addEventListener('click', () => {
+        this.nativeElement.classList.toggle('active');
+      });
+    }
+  }
 }
