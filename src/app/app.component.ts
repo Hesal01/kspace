893d64129158f530150f70d263a0cedf66136b17
jawad <karounjawad@gmail.com>
2025-01-22 10:44:05 +0100

    Ajout d'un background dans le hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import {HeroComponent} from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
